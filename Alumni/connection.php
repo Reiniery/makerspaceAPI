52d59b602189ed322e8b7637f2524defd9725b5a
Reiniery <103866541+Reiniery@users.noreply.github.com>
2024-10-06 16:37:26 -0700

    Update connection.php

--- Alumni/connection.php
+++ Alumni/connection.php
@@ -1,15 +1,12 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-
 header('Content-Type: application/json');
-
 header("Access-Control-Allow-Methods: *");
-
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $host ="localhost";
-$dbusername = "alumni";
-$dbpassword = "alumni12";
+$dbusername = "userName";
+$dbpassword = "password";
 $database ="alumni";
 try{
    $pdo = new pdo('mysql:dbname='.$database.";host=".$host.";",$dbusername,$dbpassword); 
