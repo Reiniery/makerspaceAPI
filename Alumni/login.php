4e47c4c6882dc4d04e7332dd008e907af6ad0007
Reiniery <103866541+Reiniery@users.noreply.github.com>
2024-10-06 16:37:56 -0700

    Update login.php

--- Alumni/login.php
+++ Alumni/login.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header('Content-Type: application/json');
-header("Access-Control-Allow-Methods: *");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 require 'connection.php';
 session_start();
 
